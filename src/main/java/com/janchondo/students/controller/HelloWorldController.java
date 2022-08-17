955af10a2e1fc465af50d5e3848040871d8bde63
jant1012 <anchondojesus197@gmail.com>
2022-08-17 13:37:54 -0600

    Adding Spring Security

--- src/main/java/com/janchondo/students/controller/HelloWorldController.java
+++ src/main/java/com/janchondo/students/controller/HelloWorldController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StudentController {
+public class HelloWorldController {
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "Hello world!";
